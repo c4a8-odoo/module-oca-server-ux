92a759217017e819ba891063d76506213474a0de
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-04-03 07:04:47 +0000

    HACK: Add mass_editing for migration

---
+++ mass_editing/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd. (support@serpentcs.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mass Editing",
+    "version": "16.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd., "
+    "Tecnativa, "
+    "GRAP, "
+    "Iván Todorovich, "
+    "Odoo Community Association (OCA)",
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-ux",
+    "license": "AGPL-3",
+    "summary": "Mass Editing (deprecated)",
+    "depends": [
+        "server_action_mass_edit",
+    ],
+    "data": [
+    ],
+    "demo": [],
+}
