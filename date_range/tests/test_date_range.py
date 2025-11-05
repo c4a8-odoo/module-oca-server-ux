OCA/server-ux

[IMP] date_range: Pass integration tests
If any module adds a required field on company, module fails. Executing it on
post_install, there's no problem.

date_range/tests/test_date_range.py
@@ -6,6 +6,8 @@ from odoo.exceptions import ValidationError
 
 
 class DateRangeTest(TransactionCase):
+    post_install = True
+    at_install = True
 
     def setUp(self):
         super(DateRangeTest, self).setUp()

date_range/tests/test_date_range_generator.py
@@ -7,6 +7,8 @@ from dateutil.rrule import MONTHLY
 
 
 class DateRangeGeneratorTest(TransactionCase):
+    post_install = True
+    at_install = True
 
     def setUp(self):
         super(DateRangeGeneratorTest, self).setUp()

date_range/tests/test_date_range_type.py
@@ -8,6 +8,8 @@ from odoo.exceptions import ValidationError
 
 
 class DateRangeTypeTest(TransactionCase):
+    post_install = True
+    at_install = True
 
     def setUp(self):
         super(DateRangeTypeTest, self).setUp()

c4a8-odoo/module-oca-server-ux

date_range/tests/test_date_range.py
@@ -6,6 +6,8 @@ from odoo.exceptions import ValidationError
 
 
 class DateRangeTest(TransactionCase):
+    post_install = True
+    at_install = False
 
     def setUp(self):
         super(DateRangeTest, self).setUp()

date_range/tests/test_date_range_generator.py
@@ -7,6 +7,8 @@ from dateutil.rrule import MONTHLY
 
 
 class DateRangeGeneratorTest(TransactionCase):
+    post_install = True
+    at_install = False
 
     def setUp(self):
         super(DateRangeGeneratorTest, self).setUp()

date_range/tests/test_date_range_type.py
@@ -8,6 +8,8 @@ from odoo.exceptions import ValidationError
 
 
 class DateRangeTypeTest(TransactionCase):
+    post_install = True
+    at_install = False
 
     def setUp(self):
         super(DateRangeTypeTest, self).setUp()